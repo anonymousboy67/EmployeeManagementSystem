4d69dd44fa3da22a1a8bfe44df4a68050b046c8a
anonymousboy67 <aashishad67@gmail.com>
2024-10-03 19:18:40 +0545

    first commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from 'express'
+import cors from 'cors'
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is Running on POrt ${process.env.PORT}`)
+     
+})
